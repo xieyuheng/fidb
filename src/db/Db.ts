47726be3f249d5e4e1366e755274811b39b74fb3
Xie Yuheng <xyheme@gmail.com>
2024-10-09 08:40:47 +0800

    up

--- src/db/Db.ts
+++ src/db/Db.ts
@@ -4,7 +4,7 @@ export interface Db {
   create(id: string, data: JsonObject): Promise<void>
   delete(id: string, options: { revision: string }): Promise<void>
   getOrFail(id: string): Promise<JsonObject>
-  get(id: string): Promise<JsonObject|undefined>
+  get(id: string): Promise<JsonObject | undefined>
   has(id: string): Promise<boolean>
   patch(id: string, data: JsonObject): Promise<JsonObject>
   put(id: string, data: JsonObject): Promise<JsonObject>
