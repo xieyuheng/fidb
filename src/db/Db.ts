a0022368b8576eceb69deefcbf6e2b10f410c5ed
Xie Yuheng <xyheme@gmail.com>
2024-10-10 09:28:29 +0800

    `Data` has metadata by @ prefix

--- src/db/Db.ts
+++ src/db/Db.ts
@@ -2,21 +2,20 @@ import type { JsonObject } from "../utils/Json.js"
 
 export type Id = `${string}/${string}`
 
-export type Data = JsonObject
-
 export type Metadata = {
-  revision: string
-  createdAt: number
-  updatedAt: number
+  "@id": Id
+  "@createdAt": number
+  "@updatedAt": number
 }
 
+export type Data = JsonObject & Metadata
+
 export interface Db {
-  create(id: Id, data: Data): Promise<void>
-  delete(id: Id, options: { revision: string }): Promise<void>
-  metadata(id: Id): Promise<Metadata>
+  create(id: Id, input: JsonObject): Promise<void>
+  delete(id: Id): Promise<void>
   getOrFail(id: Id): Promise<Data>
   get(id: Id): Promise<Data | undefined>
   has(id: Id): Promise<boolean>
-  patch(id: Id, data: Data): Promise<Data>
-  put(id: Id, data: Data): Promise<Data>
+  patch(id: Id, input: JsonObject): Promise<Data>
+  put(id: Id, input: JsonObject): Promise<Data>
 }
--- src/fidb/FiDb.ts
+++ src/fidb/FiDb.ts
@@ -1,53 +1,49 @@
-import { join } from "node:path"
-import type { Data, Db, Id, Metadata } from "../db/index.js"
-import { resolvePath } from "./resolvePath.js"
-
 export type FiDbConfig = {
   directory: string
 }
 
-export class FiDb implements Db {
-  constructor(public config: FiDbConfig) {}
+// export class FiDb implements Db {
+//   constructor(public config: FiDbConfig) {}
 
-  private resolveIdPath(id: Id): string {
-    const [datasetName, dataId] = id.split("/")
-    return resolvePath(
-      this.config.directory,
-      join(datasetName, "datasets", dataId),
-    )
-  }
+//   private resolveIdPath(id: Id): string {
+//     const [datasetName, dataId] = id.split("/")
+//     return resolvePath(
+//       this.config.directory,
+//       join(datasetName, "datasets", dataId),
+//     )
+//   }
 
-  async create(id: Id, data: Data): Promise<void> {
-    const [datasetName, dataId] = id.split("/")
-    join(this.resolveIdPath(id), "data.json")
-    throw new Error()
-  }
+//   async create(id: Id, data: Data): Promise<void> {
+//     const [datasetName, dataId] = id.split("/")
+//     join(this.resolveIdPath(id), "data.json")
+//     throw new Error()
+//   }
 
-  async delete(id: Id, options: { revision: string }): Promise<void> {
-    throw new Error()
-  }
+//   async delete(id: Id, options: { revision: string }): Promise<void> {
+//     throw new Error()
+//   }
 
-  async metadata(id: Id): Promise<Metadata> {
-    throw new Error()
-  }
+//   async metadata(id: Id): Promise<Metadata> {
+//     throw new Error()
+//   }
 
-  async getOrFail(id: Id): Promise<Data> {
-    throw new Error()
-  }
+//   async getOrFail(id: Id): Promise<Data> {
+//     throw new Error()
+//   }
 
-  async get(id: Id): Promise<Data | undefined> {
-    throw new Error()
-  }
+//   async get(id: Id): Promise<Data | undefined> {
+//     throw new Error()
+//   }
 
-  async has(id: Id): Promise<boolean> {
-    throw new Error()
-  }
+//   async has(id: Id): Promise<boolean> {
+//     throw new Error()
+//   }
 
-  async patch(id: Id, data: Data): Promise<Data> {
-    throw new Error()
-  }
+//   async patch(id: Id, data: Data): Promise<Data> {
+//     throw new Error()
+//   }
 
-  async put(id: Id, data: Data): Promise<Data> {
-    throw new Error()
-  }
-}
+//   async put(id: Id, data: Data): Promise<Data> {
+//     throw new Error()
+//   }
+// }
